a7538fc84d4cbbe21e0feaa90da99364b97b539d
Christian Martin dos Santos <csantos@cmsbo.leg.br>
2026-01-23 17:13:00 -0300

    Prepare 1.0.0-beta.4 release

--- src/sentineltray/__init__.py
+++ src/sentineltray/__init__.py
@@ -1,5 +1,5 @@
 """SentinelTray package."""
 
-__version__ = "1.0.0b3"
-__version_label__ = "1.0.0-beta.3"
+__version__ = "1.0.0b4"
+__version_label__ = "1.0.0-beta.4"
 __release_date__ = "23-01-2026"
